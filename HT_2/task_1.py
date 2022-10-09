eee987e71f1dc4840cf6cfa9d6952ed47df3d52a
lhetstest <yrul.rinftech@gmail.com>
2022-10-09 21:06:52 +0200

    first task

---
+++ HT_2/task_1.py
@@ -0,0 +1,8 @@
+#Write a script which accepts a sequence of comma-separated numbers from user and generates a list and a tuple with those numbers.
+y_list = []
+
+def convert(num):
+    values = input("Type values: ")
+    
+    print(values)
+    
