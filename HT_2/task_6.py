5280d252ede8b4bada237b04c78cdef7ac21bee3
lhetstest <yrul.rinftech@gmail.com>
2022-10-10 16:30:50 +0200

    task 6 added

--- HT_2/task_6.py
+++ HT_2/task_6.py
@@ -2,3 +2,12 @@
 #     e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
 #          [1, 2, 'u', 'a', 4, True] --> 5 --> False
 
+my_list = [1, 2, 'u', 'a', 4, True]
+
+value = input("Type your guess here: ")
+
+try:
+    my_list.index(value)
+    print(True)
+except ValueError:
+    print(False)
