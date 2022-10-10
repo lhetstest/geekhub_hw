f7e4905849625ffa6066e862caa0e4e3c9988349
lhetstest <yrul.rinftech@gmail.com>
2022-10-10 10:12:17 +0200

    task_5 added

---
+++ HT_2/task_5.py
@@ -0,0 +1,4 @@
+# 5. Write a script which accepts a decimal number from user and converts it to hexadecimal.
+nummer = int(input("Enter normal decimal number: "))
+
+print(hex(nummer))
---
+++ HT_2/task_6.py
@@ -0,0 +1,4 @@
+# 6. Write a script to check whether a value from user input is contained in a group of values.
+#     e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
+#          [1, 2, 'u', 'a', 4, True] --> 5 --> False
+
