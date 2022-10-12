8bee0c3abe72cf3e8b85ee2a23db3aa258f5fcbc
lhetstest <yrul.rinftech@gmail.com>
2022-10-12 20:29:11 +0200

    feat: task7 added

--- HW_3/task_7.py
+++ HW_3/task_7.py
@@ -1,3 +1,9 @@
 #  Write a script which accepts a <number>(int) from user and generates dictionary in range <number> where key is <number> and value is <number>*<number>
 #     e.g. 3 --> {0: 0, 1: 1, 2: 4, 3: 9}
 
+user_input = int(input("Enter the max value: "))
+
+my_dict = {}
+for i in range(user_input+1):
+    my_dict.update({f'{i}':f'{i*i}'})
+print(my_dict)
