c502cfca4a165bd6b5e4dfbc65fe1f986ab65550
lhetstest <yrul.rinftech@gmail.com>
2022-10-12 21:01:00 +0200

    feat: task6 added

--- HW_3/task_6.py
+++ HW_3/task_6.py
@@ -1,1 +1,13 @@
 # Write a script to get the maximum and minimum VALUE in a dictionary.
+
+my_dict = {
+    "one": 23,
+    "two": 3,
+    "three": 257,
+    "four": 39
+}
+
+minimal = min(my_dict.values())
+maximal = max(my_dict.values())
+print('This value is minimal: ', minimal)
+print('This value is maximal: ', maximal)
