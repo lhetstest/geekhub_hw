6be77b278990dd32326e679a41ef72e1a8495716
lhetstest <yrul.rinftech@gmail.com>
2022-10-13 22:47:54 +0200

    task1 added

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -1,1 +1,9 @@
 # 1. Write a script that will run through a list of tuples and replace the last value for each tuple. The list of tuples can be hardcoded. The "replacement" value is entered by user. The number of elements in the tuples must be different.
+
+list_of_tuples = [(1,2,9,"ok"), ('hey', 3), ('hello'), (True), ('Yo.', 'value')]
+
+new_element = input("Type some funny word: ")
+
+list_of_tuples[-1] = new_element
+
+print(list_of_tuples)
