c4ad5da12fcd5a65e7b7a269b7621e2da9b723db
lhetstest <yrul.rinftech@gmail.com>
2022-10-13 21:25:48 +0200

    task3 added

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -1,4 +1,8 @@
 # Write a script to concatenate the following dictionaries to create a NEW one.
-#     dict_1 = {'foo': 'bar', 'bar': 'buz'}
-#     dict_2 = {'dou': 'jones', 'USD': 36}
-#     dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+dict_1 = {'foo': 'bar', 'bar': 'buz'}
+dict_2 = {'dou': 'jones', 'USD': 36}
+dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+
+my_dict = dict_1 | dict_2 | dict_3
+
+print(my_dict)
