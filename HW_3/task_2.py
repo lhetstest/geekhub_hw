77447e7c9fb16d9cef7400ec64f1099603610a99
lhetstest <yrul.rinftech@gmail.com>
2022-10-13 21:56:10 +0200

    task2 added

--- HW_3/task_2.py
+++ HW_3/task_2.py
@@ -1,2 +1,13 @@
 # 2. Write a script to remove empty elements from a list.
 #     Test list: [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
+
+m_list=[(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []] 
+
+for item in m_list:
+    if len(item) == 0:
+        m_list.remove(item)
+
+print(m_list)
+
+print(len([]))
+# this is strange.. 
