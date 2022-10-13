f409be6f8fe333642b0b76318c3b5bc719cd5e1a
lhetstest <yrul.rinftech@gmail.com>
2022-10-13 18:35:34 +0200

    task 5 added

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -1,1 +1,16 @@
 # Write a script to remove values duplicates from dictionary. Feel free to hardcode your dictionary.
+
+my_dic = {
+    "one": 23,
+    "two": 3,
+    "three": 257,
+    "four": 39,
+    "car": "Ford",
+    "name": "Lucy",
+    "age": 240,
+    "mana": 180,
+    "married": False,
+    "mother": "Lucy"
+}
+
+print(set(my_dic.values()))
