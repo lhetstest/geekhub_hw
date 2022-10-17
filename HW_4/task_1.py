cbd86fda45a3cb597e1d953bbf0770250c83af15
lhetstest <yrul.rinftech@gmail.com>
2022-10-17 21:45:24 +0200

    task1 added

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -1,11 +1,13 @@
 # 1. Написати функцiю season, яка приймає один аргумент (номер мiсяця вiд 1 до 12) та яка буде повертати пору року, до якої цей мiсяць належить (зима, весна, лiто або осiнь). У випадку некоректного введеного значення - виводити відповідне повідомлення.
 
 def season(month):
-    try:
-        month = int(input("Please enter the number of the month: "))
-    
-        if month==12 || month<3:
-            print()
-        elif month>=3 &&
-    except error:
-        print("Type the positive number less or equal to 12")
+    if month==12 or month<3:
+        print("Winter")
+    elif month>=3 and month<5:
+        print("Spring")
+    elif month>=5 and month<9:
+        print("Summer")
+    elif month>=9 and month<12:
+        print("Autumn")
+
+season(int(input("Please enter the number of the month: ")))
